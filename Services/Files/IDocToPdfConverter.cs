275bcd3b63b3342f70a284c4efbbea66f44749b1
Alex Castner <alex@threshold.world>
2025-09-14 15:05:15 -0400

    removed a failed attempt

--- Services/Files/IDocToPdfConverter.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-using twoSaaSCore.Models;
-
-namespace twoSaaSCore.Services.Files;
-
-public interface IDocToPdfConverter
-{
-    Task<(string physicalPdfPath, string relativeBlobName)> EnsurePdfAsync(TenantFile sourceFile, string userId);
-}
